17a2ecb8f3624aede06e40b82d0424d143496284
luisjavierluna <javierlunaluis@gmail.com>
2023-01-03 21:59:40 -0600

    Almacenador y generador de nombres de Imagenes

--- BR-API/BR-API/Program.cs
+++ BR-API/BR-API/Program.cs
@@ -1,4 +1,5 @@
 using BR_API;
+using BR_API.Utilities;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
+
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -34,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
 
 app.UseCors();
 
---
+++ BR-API/BR-API/Utilities/IAlmacenadorArchivos.cs
@@ -0,0 +1,9 @@
+﻿namespace BR_API.Utilities
+{
+    public interface IAlmacenadorArchivos
+    {
+        Task BorrarArchivo(string ruta, string contenedor);
+        Task<string> EditarArchivos(string contenedor, IFormFile archivo, string ruta);
+        Task<string> GuardarArchivo(string contenedor, IFormFile archivo);
+    }
+}

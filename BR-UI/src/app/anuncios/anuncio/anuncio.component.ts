cfa6979fcf407399250563f964003f5dbf33d553
luisjavierluna <javierlunaluis@gmail.com>
2023-01-07 22:16:46 -0600

    Creando la página para mostrar la información de la propiedad

--- BR-UI/src/app/anuncios/anuncio/anuncio.component.ts
+++ BR-UI/src/app/anuncios/anuncio/anuncio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PropiedadDTO } from 'src/app/propiedades/propiedad';
+import { PropiedadesService } from 'src/app/propiedades/propiedades.service';
 
 @Component({
   selector: 'app-anuncio',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnuncioComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private propiedadesService: PropiedadesService,
+    private activatedRoute: ActivatedRoute) { }
 
+    anuncio: PropiedadDTO = {
+      id: 0,
+      titulo: '',
+      precio: 0,
+      imagen: '',
+      descripcion: '',
+      habitaciones: 0,
+      wc: 0,
+      estacionamiento: 0,
+      vendedorId: 0,
+      vendedorNombre: '',
+      vendedorApellido: '',
+    }
+  
   ngOnInit(): void {
+    this.obtenerPorId()
+  }
+
+  obtenerPorId() {
+    this.activatedRoute.params.subscribe(params => {
+      this.propiedadesService.obtenerPorId(params['id'])
+      .subscribe({
+        next: propiedad => {this.anuncio = propiedad},
+        error: errores => {console.log(errores)}
+      })
+    })
   }
 
 }
